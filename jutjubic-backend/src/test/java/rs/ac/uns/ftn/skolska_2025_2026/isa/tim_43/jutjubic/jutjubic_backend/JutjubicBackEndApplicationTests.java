c84b69bf03fd1fb0355e429d5415ed7c857c20de
Darijan Mićić <darijan.micic10@gmail.com>
2025-12-11 19:04:44 +0100

    chore: create back-end spring boot application
    References:
    https://start.spring.io/

---
+++ jutjubic-backend/src/test/java/rs/ac/uns/ftn/skolska_2025_2026/isa/tim_43/jutjubic/jutjubic_backend/JutjubicBackEndApplicationTests.java
@@ -0,0 +1,10 @@
+package rs.ac.uns.ftn.skolska_2025_2026.isa.tim_43.jutjubic.jutjubic_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JutjubicBackEndApplicationTests {
+	@Test
+	void contextLoads() {}
+}
