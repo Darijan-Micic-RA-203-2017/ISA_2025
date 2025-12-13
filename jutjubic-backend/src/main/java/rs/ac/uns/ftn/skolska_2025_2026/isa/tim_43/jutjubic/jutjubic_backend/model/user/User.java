b527ae663e611e10f08a5ff1714210d22257e0dc
Darijan Mićić <darijan.micic10@gmail.com>
2025-12-13 19:32:21 +0100

    feat(3.2): create basic user & user role classes
    References:
    https://github.com/isa-asistent/Vezbe-2025/tree/main/vezbe4/spring-security-example

---
+++ jutjubic-backend/src/main/java/rs/ac/uns/ftn/skolska_2025_2026/isa/tim_43/jutjubic/jutjubic_backend/model/user/User.java
@@ -0,0 +1,162 @@
+package rs.ac.uns.ftn.skolska_2025_2026.isa.tim_43.jutjubic.jutjubic_backend.model.user;
+
+import java.sql.Timestamp;
+import java.util.Objects;
+import java.util.Set;
+
+public class User {
+	private long id;
+	private boolean enabled;
+	private Set<UserRole> roles;
+	private String emailAddress;
+	private String username;
+	private String password;
+	private Timestamp dateOfLastPasswordReset;
+	private String firstName;
+	private String lastName;
+	private String address;
+
+	public User() {}
+
+	public User(long id, boolean enabled, Set<UserRole> roles, String emailAddress, 
+			String username, String password, Timestamp dateOfLastPasswordReset, 
+			String firstName, String lastName, String address) {
+		this.id = id;
+		this.enabled = enabled;
+		this.roles = roles;
+		this.emailAddress = emailAddress;
+		this.username = username;
+		this.password = password;
+		this.dateOfLastPasswordReset = dateOfLastPasswordReset;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public Set<UserRole> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<UserRole> roles) {
+		this.roles = roles;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Timestamp getDateOfLastPasswordReset() {
+		return dateOfLastPasswordReset;
+	}
+
+	public void setDateOfLastPasswordReset(Timestamp dateOfLastPasswordReset) {
+		this.dateOfLastPasswordReset = dateOfLastPasswordReset;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, enabled, roles, emailAddress, username, password, 
+				dateOfLastPasswordReset, firstName, lastName, address);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+
+		if (!(obj instanceof User)) {
+			return false;
+		}
+
+		User other = (User) obj;
+
+		return id == other.id && enabled == other.enabled 
+				&& Objects.equals(roles, other.roles) 
+				&& Objects.equals(emailAddress, other.emailAddress) 
+				&& Objects.equals(username, other.username) 
+				&& Objects.equals(password, other.password) 
+				&& Objects.equals(dateOfLastPasswordReset, other.dateOfLastPasswordReset) 
+				&& Objects.equals(firstName, other.firstName)  
+				&& Objects.equals(lastName, other.lastName) 
+				&& Objects.equals(address, other.address);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder("User [");
+
+		builder.append("id = ").append(id).append(", enabled = ").append(enabled)
+				.append(", roles = ").append(roles)
+				.append(", emailAddress = ").append(emailAddress)
+				.append(", username = ").append(username)
+				.append(", password = ").append(password)
+				.append(", dateOfLastPasswordReset = ").append(dateOfLastPasswordReset)
+				.append(", firstName = ").append(firstName)
+				.append(", lastName = ").append(lastName)
+				.append(", address = ").append(address)
+				.append("]");
+
+		return builder.toString();
+	}
+}
---
+++ jutjubic-backend/src/main/java/rs/ac/uns/ftn/skolska_2025_2026/isa/tim_43/jutjubic/jutjubic_backend/model/user/UserRole.java
@@ -0,0 +1,61 @@
+package rs.ac.uns.ftn.skolska_2025_2026.isa.tim_43.jutjubic.jutjubic_backend.model.user;
+
+import java.util.Objects;
+
+public class UserRole {
+	private long id;
+	private String name;
+
+	public UserRole() {}
+
+	public UserRole(long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+
+		if (!(obj instanceof UserRole)) {
+			return false;
+		}
+
+		UserRole other = (UserRole) obj;
+
+		return id == other.id && Objects.equals(name, other.name);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder("UserRole [");
+
+		builder.append("id = ").append(id).append(", name = ").append(name)
+				.append("]");
+
+		return builder.toString();
+	}
+}
