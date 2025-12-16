e2117f9c478d47846765126f77599159c8bed3f7
Darijan Mićić <darijan.micic10@gmail.com>
2025-12-16 22:47:39 +0100

    feat(3.2): turn user role into a granted authority
    References:
    https://github.com/isa-asistent/Vezbe-2025/tree/main/vezbe4/spring-security-example
    https://github.com/isa-asistent/Vezbe-2025/tree/main/vezbe3/jpa_example
    https://www.postgresql.org/docs/current/datatype-numeric.html
    https://www.postgresql.org/docs/current/datatype-character.html

--- jutjubic-backend/src/main/java/rs/ac/uns/ftn/skolska_2025_2026/isa/tim_43/jutjubic/jutjubic_backend/model/user/UserRole.java
+++ jutjubic-backend/src/main/java/rs/ac/uns/ftn/skolska_2025_2026/isa/tim_43/jutjubic/jutjubic_backend/model/user/UserRole.java
@@ -1,10 +1,36 @@
 package rs.ac.uns.ftn.skolska_2025_2026.isa.tim_43.jutjubic.jutjubic_backend.model.user;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
 import java.util.Objects;
 
-/** REFERENCE: https://github.com/isa-asistent/Vezbe-2025/tree/main/vezbe4/spring-security-example */
-public class UserRole implements Comparable<UserRole> {
+import org.springframework.security.core.GrantedAuthority;
+
+/** REFERENCES:
+ * https://github.com/isa-asistent/Vezbe-2025/tree/main/vezbe4/spring-security-example
+ * https://github.com/isa-asistent/Vezbe-2025/tree/main/vezbe3/jpa_example
+*/
+@Entity()
+@Table(name = "user_roles")
+public class UserRole implements GrantedAuthority, Comparable<UserRole> {
+	private static final long serialVersionUID = -2725148852941001777L;
+
+	/** REFERENCE: https://www.postgresql.org/docs/current/datatype-numeric.html */
+	@Id()
+	@SequenceGenerator(name = "generatorOfUserRolesIds", sequenceName = "sequenceOfUserRolesIds", 
+			initialValue = 1, allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "generatorOfUserRolesIds")
+	@Column(name = "id", nullable = false, updatable = false, columnDefinition = "bigserial")
 	private long id;
+
+	/** REFERENCE: https://www.postgresql.org/docs/current/datatype-character.html */
+	@Column(name = "name", nullable = false, columnDefinition = "text")
 	private String name;
 
 	public UserRole() {}
@@ -28,6 +54,11 @@ public class UserRole implements Comparable<UserRole> {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	@Override()
+	public String getAuthority() {
+		return name;
 	}
 
 	/** REFERENCE: https://www.geeksforgeeks.org/java/comparable-interface-in-java-with-examples/ */
