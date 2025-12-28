73417bd8bb8c4eb89ebae059bbf9023d6b070093
Darijan Mićić <darijan.micic10@gmail.com>
2025-12-28 23:08:14 +0100

    feat(3.2): impl. front-end authentication service
    References:
    https://github.com/isa-asistent/Vezbe-2025/tree/main/vezbe4/spring-security-front-app

---
+++ jutjubic-frontend/src/app/services/authentication/authentication.ts
@@ -0,0 +1,39 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+import { ObjectWithTextualContext } from '../../model/object-with-textual-context';
+import { UserCredentials } from '../../model/user/user-credentials';
+
+/** REFERENCE: https://github.com/isa-asistent/Vezbe-2025/tree/main/vezbe4/spring-security-front-app */
+@Injectable({
+  providedIn: 'root',
+})
+export class Authentication {
+  private urlOfLoggingOnMethod: string = 'http://localhost:8080/users/log-on';
+  private token: string | null;
+
+  constructor(private httpClient: HttpClient) {
+    this.token = null;
+  }
+
+  getToken(): string | null {
+    return this.token;
+  }
+
+  isTokenPresent(): boolean {
+    return this.token != undefined && this.token != null;
+  }
+
+  logOnWith(userCredentials: UserCredentials): Observable<ObjectWithTextualContext> {
+    const headersOfHttpRequestWithNonEmpthyBody: HttpHeaders = new HttpHeaders({
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    });
+
+    return this.httpClient.post<ObjectWithTextualContext>(this.urlOfLoggingOnMethod, JSON.stringify(userCredentials), {
+      headers: headersOfHttpRequestWithNonEmpthyBody
+    });
+  }
+}
