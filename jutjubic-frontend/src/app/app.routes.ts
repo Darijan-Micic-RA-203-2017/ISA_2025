0306efa83bf5aca54c8d37e7a5171d8e29357a5d
Darijan Mićić <darijan.micic10@gmail.com>
2026-01-01 07:20:12 +0100

    feat(3.2): assign route to logging on component
    References:
    https://github.com/isa-asistent/Vezbe-2025/tree/main/vezbe4/spring-security-front-app
    https://angular.dev/guide/routing/define-routes

--- jutjubic-frontend/src/app/app.routes.ts
+++ jutjubic-frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoggingOnComponent } from './components/logging-on/logging-on';
+
+/** REFERENCES:
+ * https://github.com/isa-asistent/Vezbe-2025/tree/main/vezbe4/spring-security-front-app
+ * https://angular.dev/guide/routing/define-routes
+*/
+export const routes: Routes = [
+	{ path: 'log-on', component: LoggingOnComponent }
+];
