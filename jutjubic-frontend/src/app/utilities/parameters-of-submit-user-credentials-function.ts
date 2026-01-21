0a6e4341a4974da2b3c33c99aeabed5fac152c9a
Darijan Mićić <darijan.micic10@gmail.com>
2026-01-21 21:40:07 +0100

    feat(3.2): create params. of submit function dto
    References:
    https://www.programfarmer.com/en-US/articles/2021/javascript-pass-by-value-pass-by-reference-pass-by-sharing
    https://angular.dev/essentials/signals
    https://angular.dev/guide/signals

---
+++ jutjubic-frontend/src/app/utilities/parameters-of-submit-user-credentials-function.ts
@@ -0,0 +1,56 @@
+import { WritableSignal } from "@angular/core";
+import { Router } from "@angular/router";
+
+import { MatSnackBar } from "@angular/material/snack-bar";
+
+import { UserCredentials } from "../model/user/user-credentials";
+
+export class ParametersOfSubmitUserCredentialsFunction {
+	private userCredentials: UserCredentials;
+	/** REFERENCES:
+	 * https://www.programfarmer.com/en-US/articles/2021/javascript-pass-by-value-pass-by-reference-pass-by-sharing
+	 * https://angular.dev/essentials/signals
+	 * https://angular.dev/guide/signals
+	*/
+	private isLoggingOnFormSubmitted: WritableSignal<boolean>;
+	private snackBar: MatSnackBar;
+	private router: Router;
+
+	/** REFERENCE: https://www.programfarmer.com/en-US/articles/2021/javascript-pass-by-value-pass-by-reference-pass-by-sharing */
+	constructor(userCredentials: UserCredentials, isLoggingOnFormSubmitted: WritableSignal<boolean>, snackBar: MatSnackBar, 
+			router: Router) {
+		this.userCredentials = userCredentials;
+		this.isLoggingOnFormSubmitted = isLoggingOnFormSubmitted;
+		this.snackBar = snackBar;
+		this.router = router;
+	}
+
+	public getUserCredentials(): UserCredentials {
+		return this.userCredentials;
+	}
+
+	public setUserCredentials(userCredentials: UserCredentials): void {
+		this.userCredentials = userCredentials;
+	}
+
+	public getIsLoggingOnFormSubmitted(): WritableSignal<boolean> {
+		return this.isLoggingOnFormSubmitted;
+	}
+
+	/** REFERENCES:
+	 * https://www.programfarmer.com/en-US/articles/2021/javascript-pass-by-value-pass-by-reference-pass-by-sharing
+	 * https://angular.dev/essentials/signals
+	 * https://angular.dev/guide/signals
+	*/
+	public setIsLoggingOnFormSubmitted(isLoggingOnFormSubmitted: boolean): void {
+		this.isLoggingOnFormSubmitted.set(isLoggingOnFormSubmitted);
+	}
+
+	public getSnackBar(): MatSnackBar {
+		return this.snackBar;
+	}
+
+	public getRouter(): Router {
+		return this.router;
+	}
+}
