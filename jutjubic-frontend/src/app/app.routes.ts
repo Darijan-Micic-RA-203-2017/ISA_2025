7beef8cb48f1a3968255faaa441e6812999a1719
Darijan Mićić <darijan.micic10@gmail.com>
2026-01-27 20:33:42 +0100

    feat(3.2): create route & link to reg. component
    References:
    https://github.com/isa-asistent/Vezbe-2025/tree/main/vezbe4/spring-security-front-app
    https://angular.dev/guide/routing/define-routes
    https://angular.love/angular-router-everything-you-need-to-know-about

--- jutjubic-frontend/src/app/app.routes.ts
+++ jutjubic-frontend/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 
 import { LoggingOnComponent } from './components/logging-on/logging-on';
+import { RegistrationComponent } from './components/registration/registration';
 
 /** REFERENCES:
  * https://github.com/isa-asistent/Vezbe-2025/tree/main/vezbe4/spring-security-front-app
  * https://angular.dev/guide/routing/define-routes
 */
 export const routes: Routes = [
-	{ path: 'log-on', component: LoggingOnComponent }
+	{ path: 'log-on',   component: LoggingOnComponent    },
+	{ path: 'register', component: RegistrationComponent }
 ];
